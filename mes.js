92182cf7e9185e49e70a97a3de1b6a5595388af4
JuanLBressanim <juanlbressanim@gmail.com>
2024-09-04 16:11:24 -0300

    condições: semana/mes

---
+++ mes.js
@@ -0,0 +1,42 @@
+var diaMes=parseInt(prompt("Digite um número de 1 a 12:"));
+
+if(diaMes==1){
+    console.log("Janeiro");
+
+}else if(diaMes==2){
+    console.log("Fevereiro");
+
+}else if(diaMes==3){
+    console.log("Março");
+
+}else if(diaMes==4){
+    console.log("Abril");
+
+}else if(diaMes==5){
+    console.log("Maio");
+
+}else if(diaMes==6){
+    console.log("Junho");
+
+}else if(diaMes==7){
+    console.log("Julho");
+
+}else if(diaMes==8){
+    console.log("Agosto");
+
+}else if(diaMes==9){
+    console.log("Setembro");
+
+}else if(diaMes==10){
+    console.log("Outubro");
+
+}else if(diaMes==11){
+    console.log("Novembro");
+
+}else if(diaMes==12){
+    console.log("Dezembro");
+
+
+}else{
+    console.log("Número indisponivel, digite novamente");
+};
---
+++ semana.js
@@ -0,0 +1,19 @@
+var diaSemana=parseInt(prompt("Digite um número de 1 a 7"));
+
+if(diaSemana==1){
+    console.log("Hoje é Domingo");
+}else if(diaSemana==2){
+    console.log("Hoje é Segunda-feira");
+}else if(diaSemana==3){
+    console.log("Hoje é Terça-feira");
+}else if(diaSemana==4){
+    console.log("Hoje é Quarta-feira");
+}else if(diaSemana==5){
+    console.log("Hoje é Quinta-feira");
+}else if(diaSemana==6){
+    console.log("Hoje é Sexta-feira");
+}else if(diaSemana==7){
+    console.log("Hoje é Sabado");
+}else{
+    console.log("Número indisponivel, digite novamente");
+}
